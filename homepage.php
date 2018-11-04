2df7967a21fec2e2871ff572d8942fdca5de484c
Leo-101 <40838546+Leo-101@users.noreply.github.com>
2018-11-04 13:19:56 +0500

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -1,6 +1,5 @@
 <div class="topnav">
 <link rel="stylesheet" type="text/css" href="style1.css">
-//  <a class="active" href="users_13108.php">User Table</a>
   <a href="users_13108.php">User Table</a>
   <a href="index1.php">Customer Table</a>
   <a href="salesperson_13108.php">Sales Table</a>
