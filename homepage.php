3e53880a1a71af2f4b8ebfa3bdc8526b06eaf0d1
Leo-101 <40838546+Leo-101@users.noreply.github.com>
2018-11-06 20:07:10 +0500

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" type="text/css" href="style1.css">
   <a class="active" href="users_13108.php">User Table</a>
   <a href="index1.php">Customer Table</a>
-  <a href="salesperson_13108.php">Sales Table</a>
+  <a href="salesperson_13108.php">Salesperson Table</a>
   <a href="product_13108.php">Product Table</a>
   <a class="active" href="logout.php">Logout</a>
 </div>
